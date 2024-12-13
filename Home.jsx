c35b4b9f8e36da1c98bb977fb24b55ae416537ec
dondivin123 <ndayihagazekodivin100@gmail.com>
2024-12-13 10:09:28 -0800

    react first

---
+++ Home.jsx
@@ -0,0 +1,13 @@
+const Home = () => {
+    return (
+      <div>
+        <h1 className="text-xs">This is heading one</h1>
+        <h1 className="text-lg">This is heading one</h1>
+        <h1 className="text-xl">This is heading one</h1>
+        <h1 className="text-2xl text-blue-600">This is heading one</h1>
+        <h1 className="text-4xl text-red-900">This is heading one</h1>
+        <h1>This is heading one</h1>
+      </div>
+    )
+  }
+  export default Home
